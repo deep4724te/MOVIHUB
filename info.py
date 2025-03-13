2eac14caa9ee2c93f683d3b034431bde9354856b
Deep78652 <ds7043913@gmail.com>
2025-03-13 18:52:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/744732cf25a9a29988c6e.jpg')
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/85d361ab4cb6511006022.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/86b7b7e2aa7e38f328902.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/734170f40b8169830d821.jpg'))
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://i.imgur.com/f7rjBLp.jpeg'))
 CODE = (environ.get('CODE', 'https://i.imgur.com/laMSCR7.jpeg'))
 
 #stream link shortner
