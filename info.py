b23b53078a79595d2cdbf526970efae2abcaee6f
Deep78652 <ds7043913@gmail.com>
2025-03-19 22:57:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,8 +50,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want no results messages in Log Channel
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://loyehab678:OMsUGQ9saSqdNkow@cluster0.svzv6.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "OMsUGQ9saSqdNkow")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tebewe5743:39frOJgp7kMf6b28@cluster0.w66po.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "tebewe5743")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Filmy_adda')
 
 # Verify
